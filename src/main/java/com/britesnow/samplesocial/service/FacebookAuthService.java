29c075aa8f31b847d8efb926420a2d37326ca68c
south <south@192.168.0.100>
2013-01-03 18:58:34 +0800

    . add facebook service

--- src/main/java/com/britesnow/samplesocial/service/FacebookAuthService.java
+++ src/main/java/com/britesnow/samplesocial/service/FacebookAuthService.java
@@ -1,13 +1,75 @@
 package com.britesnow.samplesocial.service;
 
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.FacebookApi;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
+import com.britesnow.samplesocial.dao.SocialIdEntityDao;
 import com.britesnow.samplesocial.entity.SocialIdEntity;
+import com.google.inject.Inject;
 
 public class FacebookAuthService implements AuthService {
+	@Inject
+	private SocialIdEntityDao socialIdEntityDao;
 
-    @Override
-    public SocialIdEntity getSocialIdEntity(Long userId) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	private static final String PROTECTED_RESOURCE_URL = "https://graph.facebook.com/me";
+	private static final Token EMPTY_TOKEN = null;
+	private static String apiKey = "504604412891475";
+	private static String apiSecret = "af295ca74435eca963a781200c79ac67";
+	private static String callBackUrl = "http://southgatetestjsppage.com:8080/sb2/oauth_fb_callback/";
 
+	@Override
+	public SocialIdEntity getSocialIdEntity(Long userId) {
+		return socialIdEntityDao.getSocialdentity(userId, "facebook");
+	}
+
+	public String getAuthorizationUrl() {
+		OAuthService service = new ServiceBuilder().provider(FacebookApi.class)
+				.apiKey(apiKey).apiSecret(apiSecret).callback(callBackUrl)
+				.build();
+		String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
+		System.out.println(authorizationUrl);
+		return authorizationUrl;
+	}
+
+	public String getAccessToken(String code) {
+		OAuthService service = new ServiceBuilder().provider(FacebookApi.class)
+				.apiKey(apiKey).apiSecret(apiSecret).callback(callBackUrl)
+				.build();
+		Verifier verifier = new Verifier(code);
+		Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+		return accessToken.getToken();
+	}
+
+	public void verfierAccessToken(String accessToken) {
+		OAuthService service = new ServiceBuilder().provider(FacebookApi.class)
+				.apiKey(apiKey).apiSecret(apiSecret).callback(callBackUrl)
+				.build();
+		OAuthRequest request = new OAuthRequest(Verb.GET,
+				PROTECTED_RESOURCE_URL);
+		service.signRequest(new Token(accessToken, apiSecret), request);
+		Response response = request.send();
+		System.out.println(response.getCode());
+		System.out.println(response.getBody());
+	}
+
+	public static void main(String[] args) {
+		try {
+			FacebookAuthService a = new FacebookAuthService();
+			// String url = a.getAuthorizationUrl();
+			// System.out.println(url);
+			String code = "AQAwCbq8WZ1BrDJmniPjNv-9ttPsDA0YyMrf0vCxnPHwpgk8tfmydBvrZtkVNsYd963wVJhPWl770RvwLMZgnhzdJ1C1zSTDWn7kwdP6VQAqt-MoeT_sba776LopHQlUL6qEtLxiAtL096WCa7k6Nk0iAc1ij-1BZVx-61tjufYyNgmSHYHklRS1_1cU530JctvyIqFbvRbMfbg8MML1-H59#_=_";
+			// String token = a.getAccessToken(code);
+			// System.out.println(token);
+			String token = "AAAHK717IUVMBAJhWdsmVCqYLZCbw7KIZCdvsK0NAsD7dDNrGnjCGm1QQHvuHvfRJVYEfSmYes0VsJpzWZCZBWKbbYoTv9aVjFomp96dkKgZDZD";
+			a.verfierAccessToken(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/com/britesnow/samplesocial/service/FacebookService.java
+++ src/main/java/com/britesnow/samplesocial/service/FacebookService.java
@@ -1,5 +1,69 @@
 package com.britesnow.samplesocial.service;
 
-public class FacebookService {
+import java.io.InputStream;
+import java.util.List;
 
+import com.restfb.BinaryAttachment;
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FacebookType;
+import com.restfb.types.Post;
+import com.restfb.types.User;
+
+public class FacebookService {
+	public User getMyInformation(String accessToken) {
+		User user = new DefaultFacebookClient(accessToken).fetchObject("me",
+				User.class);
+		return user;
+	}
+
+	public User getFriendInformation(String accessToken, String userId) {
+		User user = new DefaultFacebookClient(accessToken).fetchObject(userId,
+				User.class);
+		return user;
+	}
+
+	public List getAllFriends(String accessToken) {
+		Connection<User> myFriends = new DefaultFacebookClient(accessToken)
+				.fetchConnection("me/friends", User.class);
+		return myFriends.getData();
+	}
+
+	public List getFriendsByPage(String accessToken, Integer limit,
+			Integer offset) {
+		if (limit == null || limit <= 0) {
+			limit = 10;
+		}
+		if (offset == null || offset < 0) {
+			offset = 0;
+		}
+		Connection<User> myFriends = new DefaultFacebookClient(accessToken)
+				.fetchConnection("me/friends", User.class,
+						Parameter.with("limit", limit),
+						Parameter.with("offset", offset));
+		return myFriends.getData();
+	}
+
+	public String publish(String accessToken, String userId, String message) {
+		FacebookType publishMessageResponse = new DefaultFacebookClient(
+				accessToken).publish(userId + "/feed", FacebookType.class,
+				Parameter.with("message", message));
+		return publishMessageResponse.getId();
+	}
+
+	public String publishPhoto(String accessToken, String userId,
+			String message, InputStream is) {
+		FacebookType publishPhotoResponse = new DefaultFacebookClient(
+				accessToken).publish(userId + "/photos", FacebookType.class,
+				BinaryAttachment.with("userId", is),
+				Parameter.with("message", message));
+		return publishPhotoResponse.getId();
+	}
+
+	public List getFeedList(String accessToken, String userId) {
+		Connection<Post> myFeed = new DefaultFacebookClient(accessToken)
+				.fetchConnection("me/feed", Post.class);
+		return myFeed.getData();
+	}
 }
