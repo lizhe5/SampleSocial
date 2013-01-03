813563768e0f1393cb53e716c975c048ffb79b4c
woofgl <woofgl@gmail.com>
2013-01-03 18:22:50 +0800

    add service define, some method is not finish

---
+++ src/main/java/com/britesnow/samplesocial/mail/MailInfo.java
@@ -0,0 +1,60 @@
+package com.britesnow.samplesocial.mail;
+
+
+import javax.mail.Message;
+
+public class MailInfo {
+    private int id;
+    private long date;
+    private String from;
+    private String subject;
+    private String content;
+
+    public MailInfo(int id, long date, String from, String subject) {
+        this.id = id;
+        this.date = date;
+        this.from = from;
+        this.subject = subject;
+    }
+
+
+    public MailInfo(Message msg) {
+
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- src/main/java/com/britesnow/samplesocial/service/GContactService.java
+++ src/main/java/com/britesnow/samplesocial/service/GContactService.java
@@ -1,5 +1,99 @@
 package com.britesnow.samplesocial.service;
 
-public class GContactService {
+import com.britesnow.samplesocial.entity.User;
+import com.google.gdata.client.contacts.ContactQuery;
+import com.google.gdata.client.contacts.ContactsService;
+import com.google.gdata.data.PlainTextConstruct;
+import com.google.gdata.data.contacts.ContactEntry;
+import com.google.gdata.data.contacts.ContactFeed;
+import com.google.gdata.data.contacts.ContactGroupEntry;
+import com.google.gdata.data.contacts.ContactGroupFeed;
+import com.google.gdata.util.ServiceException;
+import com.google.inject.Singleton;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+@Singleton
+public class GContactService {
+    private final String BASE_CONTACTS_URL = "https://www.google.com/m8/feeds/contacts/default/full";
+
+    private final String BASE_GROUP_URL = "https://www.google.com/m8/feeds/groups/default/full";
+
+    public ContactGroupEntry createContactGroupEntry(User user, String name) throws Exception {
+        ContactGroupEntry group = new ContactGroupEntry();
+        group.setTitle(new PlainTextConstruct(name));
+        URL postUrl = new URL(BASE_GROUP_URL);
+        return getContactsService(user).insert(postUrl, group);
+    }
+
+    public ContactGroupEntry getContactGroupEntry(User user,String groupId) throws IOException, ServiceException {
+        URL url = new URL(BASE_GROUP_URL + "/" + groupId);
+        return getContactsService(user).getEntry(url, ContactGroupEntry.class);
+    }
+    public ContactEntry getContactEntry(User user,String contactId) throws IOException, ServiceException {
+        URL url = new URL(BASE_CONTACTS_URL + "/" + contactId);
+        return getContactsService(user).getEntry(url, ContactEntry.class);
+    }
+
+
+    public List<ContactEntry> getGroupContactResults(User user,String groupId) throws ServiceException, IOException {
+        URL feedUrl = new URL(BASE_CONTACTS_URL);
+        ContactQuery myQuery = new ContactQuery(feedUrl);
+        myQuery.setStringCustomParameter("group", groupId);
+//       myQuery.setGroup(String.format(BASE_GROUP_URL + "/" + groupId).replace("full","base"));
+//        myQuery.setGroup("https://www.google.com/m8/feeds/groups/woofgl%40gmail.com/base/6");
+        ContactFeed resultFeed = getContactsService(user).query(myQuery, ContactFeed.class);
+        return resultFeed.getEntries();
+    }
+
+    public List<ContactEntry> getContactResults(User user) throws IOException, ServiceException {
+        URL feedUrl = new URL(BASE_CONTACTS_URL);
+        ContactFeed resultFeed = getContactsService(user).getFeed(feedUrl, ContactFeed.class);
+        return resultFeed.getEntries();
+    }
+
+    public List<ContactGroupEntry> getGroupResults(User user) throws IOException, ServiceException {
+        URL feedurUrl = new URL(BASE_GROUP_URL);
+        ContactGroupFeed contactGroupFeed = getContactsService(user).getFeed(feedurUrl, ContactGroupFeed.class);
+        return contactGroupFeed.getEntries();
+    }
+
+
+
+    public ContactEntry createContact(User user,ContactInfo contact)
+            throws ServiceException, IOException {
+        ContactEntry contactEntry = contact.to();
+        //Add process
+        URL postUrl = new URL(BASE_CONTACTS_URL);
+        return getContactsService(user).insert(postUrl, contactEntry);
+
+    }
+
+
+    public void deleteGroup(User user,String groupId, String etag) throws IOException, ServiceException {
+        String url = String.format("%s/%s", BASE_GROUP_URL, groupId);
+        getContactsService(user).delete(new URL(url), etag);
+    }
+    public void deleteContact(User user,String contactId, String etag) throws IOException, ServiceException {
+        String url = String.format("%s/%s", BASE_CONTACTS_URL, contactId);
+        getContactsService(user).delete(new URL(url), etag);
+    }
+
+    public void updateContactGroupEntry(User user,String groupId, String etag, String groupName) throws IOException, ServiceException {
+        String url = String.format("%s/%s", BASE_GROUP_URL, groupId);
+        ContactGroupEntry group = new ContactGroupEntry();
+        group.setTitle(new PlainTextConstruct(groupName));
+        getContactsService(user).update(new URL(url), group, etag);
+    }
+
+    public void updateContactEntry(User user,ContactInfo contact) throws IOException, ServiceException {
+        String url = String.format("%s/%s", BASE_CONTACTS_URL, contact.getId());
+        getContactsService(user).update(new URL(url), contact.to());
+    }
+
+    private ContactsService getContactsService(User user) {
+         return null;
+    }
 }
