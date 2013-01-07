b162af08794a670da3d3b8d9f54f48332a15c462
woofgl <woofgl@gmail.com>
2013-01-07 17:00:05 +0800

    remove unuse import

--- src/main/java/com/britesnow/samplesocial/service/GoogleAuthService.java
+++ src/main/java/com/britesnow/samplesocial/service/GoogleAuthService.java
@@ -13,7 +13,6 @@ import org.scribe.oauth.OAuthService;
 
 import java.util.Date;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
